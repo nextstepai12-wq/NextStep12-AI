419c9aef7bd08073e812058d676b04f09fef0632
update product + city + country+ address <nalmadhon2@smail.ucas.edu.ps>
2026-08-24 09:26:14 +0300

    Update to the student header

--- Frontend/js/footer-index.js
+++ Frontend/js/footer-index.js
@@ -7,7 +7,7 @@
 
           '<div class="footer-col footer-brand">' +
             '<div class="brand-mark">' +
-              '<img src="../assets/logo.png" alt="NextStep AI" class="brand-logo">' +
+              '<img src="../../assets/img/logo.png" alt="NextStep AI" class="brand-logo">' +
               '<div class="brand-text">NextStep <span>AI</span></div>' +
             '</div>' +
             '<div class="logo2">' +
